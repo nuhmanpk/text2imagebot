c3ea20d212b4850da980cc86205d7c30b258647e
nuhmanpk <nuhmanpk7@gmail.com>
2023-11-03 00:38:49 +0530

    added strings and buttons

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from models import MODELS
 
+HELP = """
+● Send a Prompt , Reply /generate to the Prompt to start Image Generation
+"""
+
+START_STRING = """ **Hello {}, I'm Text to Image bot**
+Capable of running all Large image Generation Models from huggingface.
+
+`Join My Updates Channel for Getting more familiar with me`
+
+"""
+
+ABOUT = """
+● **AUTHOR :** [bughunter0](https://t.me/bughunter0) 
+● **LIBRARY :** `Pyrogram` 
+● **LANGUAGE :** `Python 3.10` 
+● **SOURCE :** [BugHunterBots](https://github.com/nuhmanpk) 
+"""
+
 CHANNEL_BUTTON = InlineKeyboardMarkup(
     [[
         InlineKeyboardButton('↗ Join Here ↗', url='https://t.me/BughunterBots')
@@ -33,23 +51,7 @@ CLOSE_BUTTON = InlineKeyboardMarkup(
     ]]
 )
 
-HELP = """
-● Send a Prompt , Reply /generate to the Prompt to start Image Generation
-"""
 
-START_STRING = """ **Hello {}, I'm Text to Image bot**
-Capable of running all Large image Generation Models from huggingface.
-
-`Join My Updates Channel for Getting more familiar with me`
-
-"""
-
-ABOUT = """
-● **AUTHOR :** [bughunter0](https://t.me/bughunter0) 
-● **LIBRARY :** `Pyrogram` 
-● **LANGUAGE :** `Python 3.10` 
-● **SOURCE :** [BugHunterBots](https://github.com/nuhmanpk) 
-"""
 BACK = [
         InlineKeyboardButton('BACK', callback_data="back")
     ]
